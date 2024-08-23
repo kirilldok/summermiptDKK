3b671c499772c56cb350350beeed07f3ad688b28
kirilldok <kirilldok007@gmail.com>
2024-08-23 15:39:57 +0300

    Add files via upload

--- squaresolvertest.c
+++ squaresolvertest.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h> 
 #include <assert.h>
+#include <string.h>
 
 const int AnyRoot = -1;
 const int No_Real_Roots = 0;
-const int Delta = 0.00000000001;
+const double Delta = 0.000001;
 const bool TRUE = 1;
-const bool FALSE = -2;
+const bool FALSE = 0;
 
 struct DataTest
 {
@@ -16,81 +17,109 @@ struct DataTest
     int nRoots_expected;
 };
 
-int SolveSquare(double a, double b, double c, double* x1, double* x2);
+void SquareEquationSolver();
+void UnitTester();
+bool DoubleEquality(double x, double y);
+bool Find_t(int count, char* arr[]);
 void RunTest(DataTest data);
-void UnitTester(double a, double b, double c);
+int SolveSquare(double a, double b, double c, double* x1, double* x2);
+void InputTester(int k);
 void Print_Roots(int nRoots, double x1, double x2);
-bool DoubleCompare(double x, double y);
 
 
 
-int main()
+int main(int argc, const char* argv[])
 {
-    // printf("Square equation solver\n");
-    // printf("Enter a,b,c:");
+    printf("Enter '-t' if you want to run test\n"
+    "Press 'Enter' if you want to run Square equation solver\n");
     
-    // int k = scanf("%lg %lg %lg", &a,  &b, &c);    
-    
-    // if (k != 3)
-    // {
-    //     printf("ERROR! TRY AGAIN \n");
-    // }
-
-    double a=NAN, b=NAN, c=NAN;
-
-    UnitTester(a, b, c);
-
-    // int nRoots = SolveSquare(a, b, c, &x1, &x2);
-
-    // Print_Roots(nRoots, x1, x2);
+    if(Find_t(argc, argv[]))
+    {
+        UnitTester();
+    }
+    else
+    {
+        SquareEquationSolver();    
+    }
     
     return 0;  
 }
 
 
 
-void UnitTester(double a, double b, double c)
+
+void SquareEquationSolver()
 {
+    printf("Square equation solver\n");
+    printf("Enter a,b,c:");
+            
+    double a=NAN, b=NAN, c=NAN;
+    int nDigits = scanf("%lg %lg %lg", &a,  &b, &c);  
 
+    InputTester(nDigits);  
+            
+    double x1 = NAN, x2 = NAN;
+    int nRoots = SolveSquare(a, b, c, &x1, &x2);
 
-    DataTest test1 = {1, 1, 0, -4, 2, -2, 2}, test2 = {2, 0, 0, 0, NAN, NAN, AnyRoot}, test3 = {3, 0, 2, 0, 0 , 0, 1},
-             test4 = {4, 1, 2, 1, -1, -1, 1}, test5 = {5, 1, 2, -3, 1, -3, 2}, test6 = {6, 1, 2, 8, NAN, NAN, No_Real_Roots},
-             test7 = {7, 1, 2.321, 32, NAN, NAN, No_Real_Roots}, test8 = {8, 0, 2.321, -0.213, 0.0917708, 0.0917708, 1},
-             test9 = {9, -9.34623, 8882.321, -42.154, 0.00474586, 950.3593, 2}, 
-             test10 = {10, 0.00001, 0.999999, -0.9999999, -100000.89999, 0.999991, 2};
-    
-    DataTest ArrDataTest[10] = {test1, test2, test3, test4, test5, test6, test7, test8, test9, test10};
-    
-    int i = NAN;
-
-    for (i=0; i<10; i++)
-    {
-        RunTest(ArrDataTest[i]);
-    }
-
-
+    Print_Roots(nRoots, x1, x2);
 }
 
 
-bool DoubleCompare(double x, double y)
+void UnitTester()
 {
-    if ((abs(x - y)) <= Delta)
+    const int nTests = 10;
+    
+    DataTest ArrDataTest[nTests] = {{1, 1, 0, -4, 2, -2, 2}, {2, 0, 0, 0, NAN, NAN, AnyRoot}, {3, 0, 2, 0, 0 , 0, 1},
+                                {4, 1, 2, 1, -1, -1, 1}, {5, 1, 2, -3, 1, -3, 2}, {6, 1, 2, 8, NAN, NAN, No_Real_Roots},
+                                {7, 1, 2.321, 32, NAN, NAN, No_Real_Roots}, {8, 0, 2.321, -0.213, 0.0917708, 0.0917708, 1},
+                                {9, -9.34623, 8882.321, -42.154, 0.00474586, 950.359305, 2}, 
+                                {10, 0.00001, 0.999999, -0.9999999, 0.999991, -100000.89999, 2}};
+
+    for (int i = 0; i < nTests; i++)
     {
-        return FALSE;
+        RunTest(ArrDataTest[i]);
     }
-    else
+}
+
+
+bool DoubleEquality(double x, double y)
+{
+    if (isnan(x))
+    {
+        return isnan(y);
+    }
+
+
+    if ((abs(x - y)) <= Delta)
     {
         return TRUE;
     }
+    else
+    {
+        return FALSE;
+    }
+}
+
+
+bool Find_t(int count, char* arr[])
+{
+    for(int i = 0; i < count; i++)
+    {
+        if (!strcmp(arr[i], "-t"))
+        {
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
 
 
 void RunTest(DataTest data)
 {
-    int count = 0;
     double x1 = NAN, x2 = NAN;
     int nRoots = SolveSquare(data.a, data.b, data.c, &x1, &x2);
-    if (nRoots != data.nRoots_expected || (!DoubleCompare(x1, data.x1_expected)) || (!DoubleCompare(x2, data.x2_expected)))
+    
+    if (nRoots != data.nRoots_expected || (!DoubleEquality(x1, data.x1_expected)) || (!DoubleEquality(x2, data.x2_expected)))
     {
         printf("ERROR Test%d, a=%lg, b=%lg, c=%lg, x1=%lg, x2=%lg, nRoots=%d \n"
         "EXPECTED: x1=%lg, x2=%lg, nRoots=%d \n", data.nTest, data.a, data.b, data.c, x1, x2, nRoots, data.x1_expected, data.x2_expected, data.nRoots_expected);    
@@ -105,24 +134,24 @@ void RunTest(DataTest data)
 int SolveSquare(double a, double b, double c, double* x1, double* x2)
 {
     assert(x1 != x2);
-    assert(a != NAN);
-    assert(b != NAN);
-    assert(c != NAN);
+    assert(!isnan(a));
+    assert(!isnan(b));
+    assert(!isnan(c));
     assert(isfinite(a));
     assert(isfinite(b));
     assert(isfinite(c));
     
-    if (a == 0) 
+    if (DoubleEquality(a, 0)) 
     {
-        if (b == 0)
+        if (DoubleEquality(b, 0))
         {
-            if (c == 0)
+            if (DoubleEquality(c, 0))
             {
                 return AnyRoot;
             }
             else
             {
-                return 0;
+                return No_Real_Roots;
             }
         }
         else 
@@ -136,9 +165,9 @@ int SolveSquare(double a, double b, double c, double* x1, double* x2)
         double D = b*b - 4*a*c;
         if (D < 0)
         {
-            return 0;
+            return No_Real_Roots;
         }
-        else if (D == 0)
+        else if (DoubleEquality(D, 0))
         {
             *x1 = *x2 = -b / (2*a);
             return 1;
@@ -150,8 +179,18 @@ int SolveSquare(double a, double b, double c, double* x1, double* x2)
             return 2;
         }
     }
-    
+    return 0;
 }
+
+
+void InputTester(int nDigits)
+{
+    if (nDigits != 3)
+    {
+        printf("ERROR! TRY AGAIN \n");
+    }
+}
+
 
 void Print_Roots(int nRoots, double x1, double x2)
 {
@@ -160,7 +199,7 @@ void Print_Roots(int nRoots, double x1, double x2)
         case AnyRoot:
             printf("Any Number");
             break;
-        case 0:
+        case No_Real_Roots:
             printf("No real roots\n");
             break;
         case 1:
@@ -173,4 +212,3 @@ void Print_Roots(int nRoots, double x1, double x2)
             printf("Error");
     }
 }
-
